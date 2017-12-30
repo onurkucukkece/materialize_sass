f8311d52d49ac273a29bd2c6b56dc435fd0c53f4
onur <onurkucukkece@gmail.com>
2017-12-30 22:58:33 +0300

    updated materialize init task

--- lib/mix/tasks/init.ex
+++ lib/mix/tasks/init.ex
@@ -12,17 +12,17 @@ defmodule Mix.Tasks.Materialize.Init do
 
 	@doc "start task"
 	def run(_) do
-		IO.puts "Installing materialize-sass-origin npm inside assets"
+		IO.puts "Installing materialize-css npm inside assets"
 		install_npm()
 		|> copy_assets
 	end
 
 	defp install_npm do
-		System.cmd "npm", ["install", "materialize-sass-origin", "--save-dev"], into: IO.stream(:stdio, :line), cd: "assets"
+		System.cmd "npm", ["install", "materialize-css", "--save-dev"], into: IO.stream(:stdio, :line), cd: "assets"
 		
-		npm_path = Path.join(~w(assets node_modules materialize-sass-origin))
+		npm_path = Path.join(~w(assets node_modules materialize-css))
 		
-		check_path(npm_path, "\nTray run npm install materialize-sass-origin --save-dev")
+		check_path(npm_path, "\nTray run npm install materialize-css --save-dev")
 
     npm_path
 	end
@@ -30,10 +30,12 @@ defmodule Mix.Tasks.Materialize.Init do
 	defp copy_assets(npm_path) do
 		assets_vendor_path = Path.join(~w(assets vendor materialize))
 		assets_static_path = Path.join(~w(assets static))
+		
+		npm_source_path = Path.join([npm_path, "dist"])
 
 		copy_resource(npm_path, assets_vendor_path, "sass")
-		copy_resource(npm_path, assets_vendor_path, "js")
-		copy_resource(npm_path, assets_static_path, "fonts")
+		copy_resource(npm_source_path, assets_vendor_path, "js")
+		copy_resource(npm_source_path, assets_static_path, "fonts")
 	end
 
 	defp copy_resource(source_path, dist_path, dir) do
