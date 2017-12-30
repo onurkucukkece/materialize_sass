cdf2cb90f135768bbd8eb90e2f2c00f85e357ea4
onur <onurkucukkece@gmail.com>
2017-12-30 19:45:41 +0300

    copy assets and copy rsource functios

--- lib/mix/tasks/init.ex
+++ lib/mix/tasks/init.ex
@@ -28,6 +28,20 @@ defmodule Mix.Tasks.Materialize.Init do
 	end
 
 	defp copy_assets(npm_path) do
+		assets_vendor_path = Path.join(~w(assets vendor materialize))
+		assets_static_path = Path.join(~w(assets static))
+
+		copy_resource(npm_path, assets_vendor_path, "sass")
+		copy_resource(npm_path, assets_vendor_path, "js")
+		copy_resource(npm_path, assets_static_path, "fonts")
+	end
+
+	defp copy_resource(source_path, dist_path, dir) do
+		res_dist_path = Path.join([dist_path, dir])
+		unless File.exists? res_dist_path do
+			File.mkdir_p res_dist_path
+		end
+		File.cp_r(Path.join([source_path, dir]), res_dist_path)
 	end
 
 	defp chek_path(path, text) do
